002a55f7f6ed2a6f4f5de9011553bc3f4dd47ce8
Tim Andersson <timalexanderandersson@hotmail.com>
2024-11-29 10:04:38 +0000

    created a posts.map for my post in explorerpage.js and title,image,description,and fontawesome icon for comments, and useeffect for fetch the posts

--- src/pages/Explorepage.js
+++ src/pages/Explorepage.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/explore.module.css";
+import axios from "axios";
 
 const Explorepage = () => {
+  const [posts, setPostinfo] = useState([])
+  const [errors, setError] = useState()
+
+  useEffect(() => {
+    const GettingPosts = async () => {
+      try{
+        const { data } = await axios.get('/post/')
+        setPostinfo(data.results)
+      } catch(err){
+        setError(errors)
+      }
+    }
+    GettingPosts();
+  },[])
+
+
+
   return (
     <div className="container d-flex justify-content-center">
       <div className={styles.centerbig}>
       <h2>Explore new reviews</h2>
         <div className={styles.colordiv}>
+          {posts.map((post, jdk)=>(
+            <div key={jdk} className={styles.bookdiv}>
+            <h3>{post.title}</h3>
+            <img src={post.image_post} alt={post.image_post} className={styles.pictures}/>
+            <p>{post.description}</p>
+            <hr></hr>
+            <i className="fa-regular fa-comment"></i>
+            </div>
+          ))}
         
         </div>
       </div>
