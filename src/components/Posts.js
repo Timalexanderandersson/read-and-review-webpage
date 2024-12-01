1f195c5bf9de6863dd5e7da38b7f0cea182ea3c3
Tim Andersson <timalexanderandersson@hotmail.com>
2024-12-01 12:12:22 +0000

    explore.module.css for title in explorepage

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -8,14 +8,13 @@ const Posts = ({posts}) => {
     <div>
         {posts.map((post, jdk)=>(
             <div key={jdk} className={styles.bookdiv}>
-              <Link to={`/post/${post.id}`}>
-              <h3 className="text-center">{post.title}</h3>
+              <Link to={`/post/${post.id}`} className={styles.headertext}>
+              <h3 >{post.title}</h3>
               </Link>
             <p className="text-center mt-2"> <strong>{post.username}</strong></p>
             <img src={post.image_post} alt={post.image_post} className={styles.pictures}/>
             <h6 className="text-center mt-2">Description</h6>
             <p className={styles.textp}>{post.description}</p>
-
             <i className="fa-regular fa-comment mt-2"></i>
             </div>
           ))}
--- src/pages/Pageinformation.js
+++ src/pages/Pageinformation.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+
 
 const Pageinformation = () => {
+    const [postsData, setpostsData] = useState({});
+    const [erros, setErros] = useState();
+    const {id} = useParams();
+
+    useEffect(() =>{
+        const gettingid = async () => {
+            try {
+                const { data } = await axios.get(`/post/${id}`)
+                setpostsData(data)
+                
+            } catch (error) {
+                setErros(erros)
+                
+            }
+        }
+        gettingid();
+    },[id])
+
   return (
+
     <div>
-        hey
+        <h1>{postsData.title}</h1>
     </div>
   )
 }
