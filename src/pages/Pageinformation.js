ad631a6c6a5fb501e97277f5454a57753662e78c
Tim Andersson <timalexanderandersson@hotmail.com>
2024-12-02 10:10:54 +0000

    created ownerpost so creater of post can edit or delete own post

--- src/pages/Pageinformation.js
+++ src/pages/Pageinformation.js
@@ -1,44 +1,61 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import styles from "../styles/Postinformation.module.css"
-import { useParams } from 'react-router-dom';
-
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import styles from "../styles/Postinformation.module.css";
+import { Link, useParams } from "react-router-dom";
+import { CurrentUserInfo } from "../users/userInformation";
 
 const Pageinformation = () => {
-    const [postsData, setpostsData] = useState({});
-    const [error, setErros] = useState();
-    const {id} = useParams();
+  const [postsData, setpostsData] = useState({});
+  const [usecomment, setcomments] = useState(null)
+  const [error, setErros] = useState();
+  const { id } = useParams();
+  const userNow = useContext(CurrentUserInfo);
 
-    useEffect(() =>{
-        const gettingid = async () => {
-            try {
-                const { data } = await axios.get(`/post/${id}`)
-                setpostsData(data)
-                
-            } catch (error) {
-                setErros(error.message)
-                
-            }
-        }
-        gettingid();
-    },[id])
+  useEffect(() => {
+
+    // getting the post id on post
+    const gettingid = async () => {
+      try {
+        const { data } = await axios.get(`/post/${id}`);
+        setpostsData(data);
+      } catch (error) {
+        setErros(error.message);
+      }
+    };
+    gettingid();
+  }, [id]);
+
+  const ownerpost = userNow?.username === postsData.username;
 
   return (
-    <div className='container d-flex justify-content-center'>
+    <div className={styles.carry}>
+      <div className={styles.container}>
         <div className={styles.bigdiv}>
-            <div className={styles.bookdiv}>
+          <div className={styles.bookdiv}>
             <h2>{postsData.title}</h2>
-            <p><strong>{postsData.username}</strong></p>
-            <img src={postsData.image_post}
-             alt={postsData.image_post}
-             className={styles.pictures}/>
-            <p>{postsData.description}</p>
-
-            </div>
+            <p>
+              <strong>{postsData.username}</strong>
+            </p>
+            <img
+              src={postsData.image_post}
+              alt={postsData.image_post}
+              className={styles.pictures}
+            />
+            <p>
+              <strong>Description</strong>
+            </p>
+            <p className={styles.textarea}>{postsData.description}</p>
+            {ownerpost && (<Link to="/" className={styles.editbutton}>
+              <i class="fa-regular fa-pen-to-square"></i>
+            </Link>)}
+          </div>
         </div>
-        
+      </div>
+      <hr className={styles.line} />
+      <p className="text-center mb-4">Need to be sign in to comment</p>
+      <hr className={styles.line} />
     </div>
-  )
-}
+  );
+};
 
-export default Pageinformation
+export default Pageinformation;
